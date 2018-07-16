f44f3e843c2d0f771fda694fa0d4d6f6b9d04539
zukonake <zukonake@protonmail.com>
2018-07-16 09:34:52 +0200

    Changed Point to Pos in common

--- src/common/entity.hpp
+++ src/common/entity.hpp
@@ -3,5 +3,5 @@
 #include <linear/vec_3.hpp>
 
 typedef float EntityCoord;
-typedef linear::Vec3<EntityCoord> EntityPoint;
+typedef linear::Vec3<EntityCoord> EntityPos;
 typedef linear::Vec3<EntityCoord> EntityVec;
--- src/common/map.hpp
+++ src/common/map.hpp
@@ -4,4 +4,4 @@
 #include <linear/vec_3.hpp>
 
 typedef I64 MapCoord;
-typedef linear::Vec3<MapCoord> MapPoint;
+typedef linear::Vec3<MapCoord> MapPos;
