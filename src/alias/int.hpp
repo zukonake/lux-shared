7dfe345624fae05f28e02392d06d4030a08a3cb9
zukonake <zukonake@protonmail.com>
2018-07-16 09:29:56 +0200

    Added sizeof asserts to alias/int.hpp

--- src/alias/int.hpp
+++ src/alias/int.hpp
@@ -14,3 +14,13 @@ typedef int32_t  I32;
 typedef int64_t  I64;
 
 typedef std::size_t SizeT;
+
+static_assert(sizeof(U8)  == 1);
+static_assert(sizeof(U16) == 2);
+static_assert(sizeof(U32) == 4);
+static_assert(sizeof(U64) == 8);
+
+static_assert(sizeof(I8)  == 1);
+static_assert(sizeof(I16) == 2);
+static_assert(sizeof(I32) == 4);
+static_assert(sizeof(I64) == 8);
