f23fc6be0a1d32a1cb86f8f8fc3a284ead00ab8e
zukonake <zukonake@protonmail.com>
2018-07-17 12:50:45 +0200

    Moved MapPos to ChunkPos and ChunkIndex functions here

--- include/lux/common/chunk.hpp
+++ include/lux/common/chunk.hpp
@@ -2,8 +2,12 @@
 
 #include <lux/alias/scalar.hpp>
 #include <lux/linear/vec_3.hpp>
+#include <lux/common/map.hpp>
 
 typedef I32 ChunkCoord;
 typedef linear::Vec3<ChunkCoord> ChunkPos;
 typedef linear::Vec3<U16>        ChunkSize;
 typedef SizeT ChunkIndex;
+
+ChunkPos   to_chunk_pos(MapPos const &pos);
+ChunkIndex to_chunk_index(MapPos const &pos);
---
+++ src/common/chunk.cpp
@@ -0,0 +1,46 @@
+#include <lux/common/chunk.hpp>
+#include <lux/consts.hpp>
+
+ChunkPos to_chunk_pos(MapPos const &pos)
+{
+    auto const &SIZE = consts::CHUNK_SIZE;
+    ChunkPos result = pos;
+    if(result.x < 0)
+    {
+        result.x -= SIZE.x - 1;
+    }
+    if(result.y < 0)
+    {
+        result.y -= SIZE.y - 1;
+    }
+    if(result.z < 0)
+    {
+        result.z -= SIZE.z - 1;
+    }
+    result.x /= SIZE.x;
+    result.y /= SIZE.y;
+    result.z /= SIZE.z;
+    return result;
+}
+
+ChunkIndex to_chunk_index(MapPos const &pos)
+{
+    auto const &SIZE = consts::CHUNK_SIZE;
+    MapPos result = pos;
+    result.x %= SIZE.x;
+    result.y %= SIZE.y;
+    result.z %= SIZE.z;
+    if(result.x < 0)
+    {
+        result.x += SIZE.x;
+    }
+    if(result.y < 0)
+    {
+        result.y += SIZE.y;
+    }
+    if(result.z < 0)
+    {
+        result.z += SIZE.z;
+    }
+    return (ChunkIndex)(result.x + (result.y * SIZE.x) + (result.z * SIZE.x * SIZE.y));
+}
