cb19e97bcac56df8686560285a18c1acf66de288
zukonake <zukonake@protonmail.com>
2018-09-06 22:59:36 +0200

    Marked lux::error with [[noreturn]]

--- include/lux/common.hpp
+++ include/lux/common.hpp
@@ -8,7 +8,7 @@ namespace lux
 {
 
 template<typename ...Args>
-inline void error(const char *prefix, const char *fmt, Args &&...args)
+[[noreturn]] void error(const char *prefix, const char *fmt, Args &&...args)
 {
     util::log(prefix, util::CRITICAL, fmt, args...);
     std::quick_exit(EXIT_FAILURE);
