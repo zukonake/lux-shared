bfed20bcb6cdf5646e7b4f2e3d0b76c26ae2bd3e
zukonake <zukonake@protonmail.com>
2018-09-14 19:36:33 +0200

    Added get_ip

--- include/lux/net.hpp
+++ include/lux/net.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <enet/enet.h>
+//
 #include <lux/common.hpp>
 
 U8 constexpr NET_VERSION_MAJOR = 0;
@@ -13,3 +15,8 @@ U8  constexpr SIGNAL_CHANNEL = 0x02;
 
 Uns constexpr SERVER_NAME_LEN = 32;
 Uns constexpr CLIENT_NAME_LEN = 16;
+
+inline U8* get_ip(ENetPeer* peer) {
+    static_assert(sizeof(peer->address.host) == 4);
+    return (U8*)&peer->address.host;
+}
