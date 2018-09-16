229176d45cafa910d1a3eeb10f257d67ef9904a9
zukonake <zukonake@protonmail.com>
2018-09-17 01:10:02 +0200

    Added more info on send_packet error

--- src/net/enet.cpp
+++ src/net/enet.cpp
@@ -6,14 +6,24 @@
 
 LUX_MAY_FAIL send_packet(ENetPeer* peer, ENetHost *host,
                          Slice<U8> slice, U8 channel, U32 flags) {
+    auto log_fail= [&]() {
+        U8* ip = get_ip(peer->address);
+        LUX_LOG("    data size: %zuB", slice.len);
+        LUX_LOG("    channel: %u", channel);
+        LUX_LOG("    flags: %x", flags);
+        LUX_LOG("    peer ip: %u.%u.%u.%u", ip[0], ip[1], ip[2], ip[4]);
+        LUX_LOG("    peer data: %zu", (SizeT)peer->data);
+    };
     ENetPacket* pack = enet_packet_create(slice.beg, slice.len,
         ENET_PACKET_FLAG_NO_ALLOCATE | flags);
     if(pack == nullptr) {
         LUX_LOG("failed to create enet packet"); //@TODO more info
+        log_fail();
         return LUX_FAIL;
     }
     if(enet_peer_send(peer, channel, pack) < 0) {
         LUX_LOG("failed to send enet packet"); //@TODO more info
+        log_fail();
         return LUX_FAIL;
     }
     enet_host_flush(host);
