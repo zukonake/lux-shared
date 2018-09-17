f8fc2ccdae06025135c8af663201632c60e04ea6
zukonake <zukonake@protonmail.com>
2018-09-17 11:17:53 +0200

    Reworked enet helper functions

--- include/lux_shared/net/enet.hpp
+++ include/lux_shared/net/enet.hpp
@@ -3,7 +3,6 @@
 #include <enet/enet.h>
 #include <lux_shared/common.hpp>
 
-LUX_MAY_FAIL send_packet(ENetPeer* peer, Slice<U8> slice, U8 channel, U32 flags);
-LUX_MAY_FAIL send_init(ENetPeer* peer, Slice<U8> slice);
-LUX_MAY_FAIL send_tick(ENetPeer* peer, Slice<U8> slice);
-LUX_MAY_FAIL send_signal(ENetPeer* peer, Slice<U8> slice);
+LUX_MAY_FAIL send_packet(ENetPeer* peer, ENetPacket* pack, U8 channel);
+LUX_MAY_FAIL create_reliable_pack(ENetPacket*& pack, SizeT sz);
+LUX_MAY_FAIL create_unreliable_pack(ENetPacket*& pack, SizeT sz);
--- src/net/enet.cpp
+++ src/net/enet.cpp
@@ -4,39 +4,34 @@
 #include <lux_shared/net/common.hpp>
 #include <lux_shared/net/enet.hpp>
 
-LUX_MAY_FAIL send_packet(ENetPeer* peer, Slice<U8> slice, U8 channel, U32 flags) {
-    auto log_fail= [&]() {
-        U8* ip = get_ip(peer->address);
-        LUX_LOG("    data size: %zuB", slice.len);
-        LUX_LOG("    channel: %u", channel);
-        LUX_LOG("    flags: %x", flags);
-        LUX_LOG("    peer ip: %u.%u.%u.%u", ip[0], ip[1], ip[2], ip[4]);
-        LUX_LOG("    peer data: %zu", (SizeT)peer->data);
-    };
-    ENetPacket* pack = enet_packet_create(slice.beg, slice.len,
-        ENET_PACKET_FLAG_NO_ALLOCATE | flags);
+LUX_MAY_FAIL create_pack(ENetPacket*& pack, SizeT sz, U32 flags) {
+    pack = enet_packet_create(nullptr, sz, flags);
     if(pack == nullptr) {
-        LUX_LOG("failed to create enet packet"); //@TODO more info
-        log_fail();
+        LUX_LOG("failed to create enet packet");
+        LUX_LOG("    data size: %zuB", sz);
+        LUX_LOG("    flags: %x", flags);
         return LUX_FAIL;
     }
-    if(enet_peer_send(peer, channel, pack) < 0) {
-        LUX_LOG("failed to send enet packet"); //@TODO more info
-        log_fail();
-        return LUX_FAIL;
-    }
-    enet_host_flush(peer->host);
     return LUX_OK;
 }
 
-LUX_MAY_FAIL send_init(ENetPeer* peer, Slice<U8> slice) {
-    return send_packet(peer, slice, INIT_CHANNEL, ENET_PACKET_FLAG_RELIABLE);
+LUX_MAY_FAIL send_packet(ENetPeer* peer, ENetPacket* pack, U8 channel) {
+    if(enet_peer_send(peer, channel, pack) < 0) {
+        U8* ip = get_ip(peer->address);
+        LUX_LOG("failed to send enet packet");
+        LUX_LOG("    data size: %zuB", pack->dataLength);
+        LUX_LOG("    channel: %u", channel);
+        LUX_LOG("    peer ip: %u.%u.%u.%u", ip[0], ip[1], ip[2], ip[4]);
+        LUX_LOG("    peer data: %zu", (SizeT)peer->data);
+        return LUX_FAIL;
+    }
+    return LUX_OK;
 }
 
-LUX_MAY_FAIL send_tick(ENetPeer* peer, Slice<U8> slice) {
-    return send_packet(peer, slice, TICK_CHANNEL, ENET_PACKET_FLAG_UNSEQUENCED);
+LUX_MAY_FAIL create_reliable_pack(ENetPacket*& pack, SizeT sz) {
+    return create_pack(pack, sz, ENET_PACKET_FLAG_RELIABLE);
 }
 
-LUX_MAY_FAIL send_signal(ENetPeer* peer, Slice<U8> slice) {
-    return send_packet(peer, slice, SIGNAL_CHANNEL, ENET_PACKET_FLAG_RELIABLE);
+LUX_MAY_FAIL create_unreliable_pack(ENetPacket*& pack, SizeT sz) {
+    return create_pack(pack, sz, ENET_PACKET_FLAG_UNSEQUENCED);
 }
