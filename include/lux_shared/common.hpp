300282488f79f9040a6d3f8b2e8bef55b3d3c8ec
zukonake <zukonake@protonmail.com>
2018-10-16 05:01:23 +0200

    Added f32_cmp for float comparison

--- include/lux_shared/common.hpp
+++ include/lux_shared/common.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <cmath>
+#include <cfloat>
+//
 #include <lux_shared/int.hpp>
 #include <lux_shared/macro.hpp>
 #include <lux_shared/memory.hpp>
 #include <lux_shared/vec.hpp>
 #include <lux_shared/containers.hpp>
+
+inline bool f32_cmp(F32 a, F32 b) {
+    return std::fabs(a - b) < FLT_EPSILON;
+}
