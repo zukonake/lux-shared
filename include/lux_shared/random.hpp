eeb5c0bec3fb78973cd19f26f76b53665ac4fa6d
zukonake <zukonake@protonmail.com>
2018-11-07 16:25:38 +0100

    Some improvements to random functions

--- include/lux_shared/random.hpp
+++ include/lux_shared/random.hpp
@@ -5,6 +5,7 @@
 #include <xxhash/xxhash.h>
 //
 #include <lux_shared/int.hpp>
+#include <lux_shared/macro.hpp>
 
 extern U64 random_seed;
 
@@ -16,17 +17,30 @@ U64 lux_rand(Args const& ...args) {
 }
 
 template<typename... Args>
-U64 lux_rands(U64 span, Args const& ...args) {
-    return lux_rand(args...) % span;
+U64 lux_randm(U64 max, Args const& ...args) {
+    return lux_rand(args...) % max;
 }
 
 template<typename... Args>
-U64 lux_randms(U64 min, U64 span, Args const& ...args) {
-    return lux_rands(span, args...) + min;
+U64 lux_randmm(U64 min, U64 max, Args const& ...args) {
+    LUX_ASSERT(min < max);
+    return lux_randm(max - min, args...) + min;
+}
+
+template<typename... Args>
+I64 lux_randsmm(I64 min, I64 max, Args const& ...args) {
+    LUX_ASSERT(min < max);
+    return (I64)lux_randm(max - min, args...) + min;
 }
 
 template<typename... Args>
 F32 lux_randf(Args const& ...args) {
     constexpr SizeT max = 1 << 24;
-    return (F32)lux_rands(max, args...) / (F32)(max - 1);
+    return (F32)lux_randm(max, args...) / (F32)(max - 1);
+}
+
+template<typename... Args>
+F32 lux_randfmm(F32 min, F32 max, Args const& ...args) {
+    LUX_ASSERT(min < max);
+    return min + lux_randf(args...) * (max - min);
 }
