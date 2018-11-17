e0d318b5b10d070aaa071f08af0aeec11bf920a4
zukonake <zukonake@protonmail.com>
2018-11-17 23:05:43 +0100

    Added rcp (reciprocal) to math.h

--- include/lux_shared/math.hpp
+++ include/lux_shared/math.hpp
@@ -10,4 +10,9 @@ inline bool f32_cmp(F32 a, F32 b) {
     return std::abs(a - b) < FLT_EPSILON;
 }
 
+template<typename T>
+inline T rcp(T a) {
+    return (T)1 / a;
+}
+
 F32 constexpr tau = 6.28318530717958647692528676655900576839433879875021;
