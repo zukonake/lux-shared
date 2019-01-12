e37b559efd53802798731b69debd6a1d7a678fda
zukonake <zukonake@protonmail.com>
2019-01-12 17:10:28 +0100

    Split 2d and 3d offsets

--- include/lux_shared/offsets.hpp
+++ include/lux_shared/offsets.hpp
@@ -17,23 +17,56 @@ template<typename T>
 T constexpr quad_idxs[] = {0, 1, 2, 2, 3, 0};
 
 template<typename T>
-Vec2<T> constexpr manhattan[] =
+Vec2<T> constexpr manhattan_2d[] =
              {{0, -1},
      {-1, 0}, {0,  0}, {1, 0},
               {0,  1}};
 
 template<typename T>
-Vec2<T> constexpr manhattan_hollow[] =
+Vec3<T> constexpr manhattan[] =
+    {{0, -1, 0}, {-1, 0, 0}, {0, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, -1},
+     {0, 0, 1}};
+
+template<typename T>
+Vec2<T> constexpr manhattan_hollow_2d[] =
     {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
 
 template<typename T>
-Vec2<T> constexpr chebyshev[] =
+Vec3<T> constexpr manhattan_hollow[] =
+    {{0, -1, 0}, {-1, 0, 0}, {1, 0, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}};
+
+template<typename T>
+Vec2<T> constexpr chebyshev_2d[] =
     {{-1, -1}, { 0, -1}, { 1, -1},
      {-1,  0}, { 0,  0}, { 1,  0},
      {-1,  1}, { 0,  1}, { 1,  1}};
 
 template<typename T>
-Vec2<T> constexpr chebyshev_hollow[] =
+Vec2<T> constexpr chebyshev_hollow_2d[] =
     {{-1, -1}, { 0, -1}, { 1, -1},
      {-1,  0}          , { 1,  0},
      {-1,  1}, { 0,  1}, { 1,  1}};
+
+template<typename T>
+Vec3<T> constexpr chebyshev[] =
+    {{-1, -1, -1}, { 0, -1, -1}, { 1, -1, -1},
+     {-1,  0, -1}, { 0,  0, -1}, { 1,  0, -1},
+     {-1,  1, -1}, { 0,  1, -1}, { 1,  1, -1},
+     {-1, -1,  0}, { 0, -1,  0}, { 1, -1,  0},
+     {-1,  0,  0}, { 0,  0,  0}, { 1,  0,  0},
+     {-1,  1,  0}, { 0,  1,  0}, { 1,  1,  0},
+     {-1, -1,  1}, { 0, -1,  1}, { 1, -1,  1},
+     {-1,  0,  1}, { 0,  0,  1}, { 1,  0,  1},
+     {-1,  1,  1}, { 0,  1,  1}, { 1,  1,  1}};
+
+template<typename T>
+Vec2<T> constexpr chebyshev_hollow[] =
+    {{-1, -1, -1}, { 0, -1, -1}, { 1, -1, -1},
+     {-1,  0, -1}, { 0,  0, -1}, { 1,  0, -1},
+     {-1,  1, -1}, { 0,  1, -1}, { 1,  1, -1},
+     {-1, -1,  0}, { 0, -1,  0}, { 1, -1,  0},
+     {-1,  0,  0},               { 1,  0,  0},
+     {-1,  1,  0}, { 0,  1,  0}, { 1,  1,  0},
+     {-1, -1,  1}, { 0, -1,  1}, { 1, -1,  1},
+     {-1,  0,  1}, { 0,  0,  1}, { 1,  0,  1},
+     {-1,  1,  1}, { 0,  1,  1}, { 1,  1,  1}};
